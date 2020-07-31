db5104d9fc047e309d6dfcd5ad3962d18b3c0fb6
Mike Nomitch <mnomitch@hashicorp.com>
2020-07-31 15:04:45 -0700

    Trying it out

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
-main.tf
+provider "aws" {
+  version = "3.0.0"
+}
+
+resource "aws_s3_bucket" "b" {
+  bucket = "s3-website-test.hashicorp.com"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
+}
